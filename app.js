67be32abff10f2d004bbd1d41fcb447fc46b6485
Naresh-Jandrajupalli42 <nareshjandrajupalli42@gmail.com>
2024-07-01 17:02:40 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const express = require('require')
+const express = require('express')
 const app = express()
 app.get('/gadgets', (request, response) => {
   response.sendFile('./gadgets.html', {root: __dirname})
